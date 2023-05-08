853ae3ce6d570f1d8887bc7f161a8ce0bf1a50f9
Jose Luiz Rodrigues <zeluizr@me.com>
2023-05-08 14:10:35 +0200

    modal second part

--- react/components/Modal.jsx
+++ react/components/Modal.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useCssHandles } from "vtex.css-handles";
+
+import "../styles/Modal.css";
+
+const CSS_HANDLES = ["modalOverlay", "modal", "modalClose", "modalContent"];
 
 export const Modal = ({ isOpen, onClose }) => {
+  const handles = useCssHandles(CSS_HANDLES);
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <button className="modal-close" onClick={onClose}>
+    <div className={`${handles.modalOverlay}`}>
+      <div className={`${handles.modal}`}>
+        <button className={`${handles.modalClose}`} onClick={onClose}>
           X
         </button>
-        <div>
+        <div className={`${handles.modalContent}`}>
           <h1>Aqui vai o conteudo</h1>
         </div>
       </div>
